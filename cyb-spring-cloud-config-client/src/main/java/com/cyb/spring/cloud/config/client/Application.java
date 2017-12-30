8b8476da263088cc78c2dbbf4c34c4c3372ece98
hustcyb <hustcyb@gmail.com>
2017-12-30 22:58:16 +0800

    提交配置中心客户端和服务端代码

---
+++ cyb-spring-cloud-config-client/src/main/java/com/cyb/spring/cloud/config/client/Application.java
@@ -0,0 +1,24 @@
+package com.cyb.spring.cloud.config.client;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@Value("${foo}")
+	private String foo;
+
+	@RequestMapping("hello")
+	public String hello() {
+		return "Hello, " + foo;
+	}
+}
