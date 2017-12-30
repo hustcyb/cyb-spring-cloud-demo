b24a901fe374a5a9f59bf708606bf55f7c1ff3a5
hustcyb <hustcyb@gmail.com>
2017-12-30 23:45:06 +0800

    将配置中心服务端和客户端接入服务注册中心

--- cyb-spring-cloud-config-client/src/main/java/com/cyb/spring/cloud/config/client/Application.java
+++ cyb-spring-cloud-config-client/src/main/java/com/cyb/spring/cloud/config/client/Application.java
@@ -3,9 +3,11 @@ package com.cyb.spring.cloud.config.client;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@EnableEurekaClient
 @RestController
 @SpringBootApplication
 public class Application {
